69619e0cde0ac02bc5e36d26038e7521e64b1d0c
ParicBat <47944384+ParicBat@users.noreply.github.com>
2020-02-14 00:08:12 +0100

    Made the script suitable for external use

--- MakeGraph.py
+++ MakeGraph.py
@@ -1,50 +1,55 @@
 import tkinter as tk
 from tkinter import filedialog
-import bokeh, bokeh.plotting, bokeh.layouts, json
-
-# File Dialog
-
-root = tk.Tk()
-root.withdraw()
-
-file_path = filedialog.askopenfilename()
+import bokeh, bokeh.plotting, bokeh.layouts, json, os, webbrowser
 
 # Generating graph
+def MakeGraph(file, output):
+    """Creates a html document with graphs of the results. Outputs the location of the file"""
+    data = json.load(file)
 
-data = json.load(open(file_path, "r"))
+    x = range(0, len(data))
 
-x = range(0, len(data))
+    y_fail = [0]
+    y_success = [0]
 
-y_fail = [0]
-y_success = [0]
+    fail = 0
+    success = 0
 
-fail = 0
-success = 0
+    line_graph = bokeh.plotting.figure(title="Succeeded and Failed tests (Line)", toolbar_location=None, tools="", x_axis_label="Checked Tests", y_axis_label="Number of Tests")
+    bar_graph = bokeh.plotting.figure(x_range=["Succeeded", "Failed"], title="Succeeded and Failed tests (Bar)", toolbar_location=None, tools="", y_axis_label="Number of Tests")
 
-bokeh.plotting.output_file("Graph.html")
+    for check in data:
+        try:
+            if check["preparation"] and check["initial_scan"] and check["remediation"] and check["final_scan"]:
+                success += 1
+            else:
+                assert False
+        except:
+            fail += 1
+        
+        y_fail.append(fail)
+        y_success.append(success)
 
-line_graph = bokeh.plotting.figure(title="Succeeded and Failed tests (Line)", toolbar_location=None, tools="", x_axis_label="Checked Tests", y_axis_label="Number of Tests")
-bar_graph  = bokeh.plotting.figure(x_range=["Succeeded", "Failed"], title="Succeeded and Failed tests (Bar)", toolbar_location=None, tools="", y_axis_label="Number of Tests")
+    line_graph.line(x, y_success, legend_label="Succeeded", line_color="blue")
+    line_graph.line(x, y_fail, legend_label="Failed", line_color="red")
 
-for check in data:
-    try:
-        if check["preparation"] and check["initial_scan"] and check["remediation"] and check["final_scan"]:
-            success += 1
-        else:
-            assert False
-    except:
-        fail += 1
-    
-    y_fail.append(fail)
-    y_success.append(success)
+    bar_graph.vbar(x=["Succeeded", "Failed"], top=[success, fail], width=0.5, color=["blue", "red"])
 
-line_graph.line(x, y_success, legend_label="Succeeded", line_color="blue")
-line_graph.line(x, y_fail, legend_label="Failed", line_color="red")
+    line_graph.legend.location = "top_left"
 
-bar_graph.vbar(x=["Succeeded", "Failed"], top=[success, fail], width=0.5, color=["blue", "red"])
+    plot = bokeh.layouts.row(line_graph, bar_graph)
 
-line_graph.legend.location = "top_left"
+    bokeh.plotting.output_file(output)
+    return bokeh.plotting.save(plot)
 
-plot = bokeh.layouts.row(line_graph, bar_graph)
+def ShowGraph(file, output):
+    """Shows a html document with graphs of the results"""
+    webbrowser.open('file://' + os.path.realpath(MakeGraph(file, output)))
 
-bokeh.plotting.show(plot)
+# File Dialog
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+    ShowGraph(open(file_path, "r"), "Graph.html")
