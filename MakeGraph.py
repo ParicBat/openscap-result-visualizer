24383a00c4b31fe2a673907796ad9b81021d3e34
ParicBat <47944384+ParicBat@users.noreply.github.com>
2020-02-20 15:11:08 +0100

    Made function checkTest() + shortened the script to test tests

--- MakeGraph.py
+++ MakeGraph.py
@@ -6,48 +6,43 @@ import bokeh
 import bokeh.plotting
 import bokeh.layouts
 
-# Generating graph
-def MakeGraph(file, output):
+def checkTest(test):
+    """Checks if the test has failed or succeeded. Returns True if suceeded."""
+    try:
+        if test["final_scan"]:
+            return True
+    except:
+        pass
+    return False
+
+def makeGraph(file, output):
     """Creates a html document with graphs of the results. Outputs the location of the file"""
     data = json.load(file)
-
-    x = range(0, len(data))
-
-    y_fail = [0]
-    y_success = [0]
 
     fail = 0
     success = 0
 
-    line_graph = bokeh.plotting.figure(title="Succeeded and Failed tests (Line)", toolbar_location=None, tools="", x_axis_label="Checked Tests", y_axis_label="Number of Tests")
-    bar_graph = bokeh.plotting.figure(x_range=["Succeeded", "Failed"], title="Succeeded and Failed tests (Bar)", toolbar_location=None, tools="", y_axis_label="Number of Tests")
+    bar_graph = bokeh.plotting.figure(
+        x_range=["Succeeded", "Failed"],
+        title="Succeeded and Failed tests (Bar)",
+        toolbar_location=None, tools="",
+        y_axis_label="Number of Tests")
 
-    for check in data:
-        try:
-            if check["preparation"] and check["initial_scan"] and check["remediation"] and check["final_scan"]:
-                success += 1
-            else:
-                assert False
-        except:
+    for test in data:
+        if checkTest(test):
+            success += 1
+        else:
             fail += 1
-        y_fail.append(fail)
-        y_success.append(success)
-
-    line_graph.line(x, y_success, legend_label="Succeeded", line_color="blue")
-    line_graph.line(x, y_fail, legend_label="Failed", line_color="red")
-
     bar_graph.vbar(x=["Succeeded", "Failed"], top=[success, fail], width=0.5, color=["blue", "red"])
 
-    line_graph.legend.location = "top_left"
-
-    plot = bokeh.layouts.row(line_graph, bar_graph)
+    plot = bokeh.layouts.row(bar_graph)
 
     bokeh.plotting.output_file(output)
     return bokeh.plotting.save(plot)
 
 def ShowGraph(file, output):
     """Shows a html document with graphs of the results"""
-    webbrowser.open('file://' + os.path.realpath(MakeGraph(file, output)))
+    webbrowser.open('file://' + os.path.realpath(makeGraph(file, output)))
 
 if __name__ == "__main__":
     output_file = "Graph.html"
@@ -55,4 +50,3 @@ if __name__ == "__main__":
         if len(sys.argv) > 2:
             output_file = sys.argv[2]
         ShowGraph(open(sys.argv[1]), output_file)
-        
