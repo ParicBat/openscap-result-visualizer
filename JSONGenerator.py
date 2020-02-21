40a23a8455ba413ecc38c0b6328b888e81fb6841
Tom <paric.cz@email.cz>
2020-02-21 17:04:06 +0100

    Line graph from multiple files and more

--- JSONGenerator.py
+++ JSONGenerator.py
@@ -4,7 +4,7 @@ import json
 
 f = open(sys.argv[1], "w")
 tests = []
-testtypes = [{"final_scan":True}, {"final_scan":False}]
+testtypes = [{"final_scan": True}, {"final_scan": False}]
 
 for i in range(random.randint(2, 100)):
     tests.append(testtypes[random.randint(0, len(testtypes)-1)])
